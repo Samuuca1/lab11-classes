77dd0a6bfd6e5609a454cf25f1e8f24891a471ec
Samuuca1 <samuelchaves_2012@hotmail.com>
2025-08-14 07:57:07 -0400

    Created method for getTotalValue

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-
+//Basic ProductProperties Class with a constructor
 
 class ProductProperties {
     constructor(name, price, quantity) {
         this.name = name;
         this.price = price;
         this.quantity = quantity;
+    }
+
+//Method to return the total value (price * quantity)
+    getTotalValue() {
+    return this.price * this.quantity;
     }
 }
