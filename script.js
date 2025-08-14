e4610328f3a2f11446b45e3bcf2caad18253d8e2
Samuuca1 <samuelchaves_2012@hotmail.com>
2025-08-14 08:31:35 -0400

    Finished the static method

--- script.js
+++ script.js
@@ -17,24 +17,35 @@ class ProductProperties {
         return `Product: ${this.name}, Price: CAD${this.price.toFixed(2)}, Quantity: ${this.quantity}`;
     }
 
+
+    // Static method to apply discount
     static applyDiscount(products, discount) {
         if (!Array.isArray(products) || typeof discount !== "number" || discount < 0 || discount > 1) {
             throw new Error("Invalid input for applyDiscount.");
         }
+
+        products.forEach(product => {
+            if (product instanceof ProductProperties) {
+                product.price -= product.price * discount;
+            }
+        });
     }
 }
 
 
-//Creating a sub class and its expansion constructor
 
-class PerishableProductProperties extends ProductProperties {
-    constructor(name, price, quantity, expirationDate) {
-        super(name, price, quantity); // calling parent constructor
-        this.expirationDate = expirationDate;
-    }
 
-    toString() {
-        return `${super.toString()}, Expiration Date: ${this.expirationDate}`;
-    }
-}
+
+        //Creating a sub class and its expansion constructor
+
+        class PerishableProductProperties extends ProductProperties {
+            constructor(name, price, quantity, expirationDate) {
+                super(name, price, quantity); // calling parent constructor
+                this.expirationDate = expirationDate;
+            }
+
+            toString() {
+                return `${super.toString()}, Expiration Date: ${this.expirationDate}`;
+            }
+        }
 
