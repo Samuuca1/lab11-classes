e41a54262c224b74c899a70e83d36b68e39a3d3f
Samuuca1 <samuelchaves_2012@hotmail.com>
2025-08-14 08:06:19 -0400

    Created method to String to return representation of product

--- script.js
+++ script.js
@@ -7,8 +7,19 @@ class ProductProperties {
         this.quantity = quantity;
     }
 
-//Method to return the total value (price * quantity)
+    //Method to return the total value (price * quantity)
     getTotalValue() {
-    return this.price * this.quantity;
+        return this.price * this.quantity;
+    }
+
+    //Returns a string representation of the product
+    toString() {
+        return `Product: ${this.name}, Price: CAD${this.price.toFixed(2)}, Quantity: ${this.quantity}`;
     }
 }
+
+//Test code
+
+const apple = new ProductProperties("Apple", 2.5, 50);
+console.log(apple.toString()); 
+console.log("Valor Total:", maca.getTotalValue()); 
